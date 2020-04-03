708554e3f7d57404bb4d0f8362cb782847c103fb
Bruce Wells <brucekwells@gmail.com>
2020-04-03 13:20:39 -0400

    Script to automatically update subtrees

---
+++ updateSubtrees.bat
@@ -0,0 +1,6 @@
+git subtree pull --prefix subtrees/codedance/jquery.AreYouSure https://github.com/codedance/jquery.AreYouSure.git master --squash
+git subtree pull --prefix subtrees/devbridge/jQuery-Autocomplete https://github.com/devbridge/jQuery-Autocomplete.git master --squash
+git subtree pull --prefix subtrees/JeremyFagis/dropify https://github.com/JeremyFagis/dropify.git master --squash
+git subtree pull --prefix subtrees/kenwheeler/slick https://github.com/kenwheeler/slick.git master --squash
+git subtree pull --prefix subtrees/najlepsiwebdesigner/foundation-datepicker https://github.com/najlepsiwebdesigner/foundation-datepicker.git master --squash
+git subtree pull --prefix subtrees/ten1seven/what-input https://github.com/ten1seven/what-input.git master --squash
