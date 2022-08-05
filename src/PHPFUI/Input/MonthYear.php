eb2c8234da969a07b2391392b81947a1d5a6e646
Bruce Wells <brucekwells@gmail.com>
2022-08-05 09:51:37 -0400

    Avoid parsing null

--- src/PHPFUI/Input/MonthYear.php
+++ src/PHPFUI/Input/MonthYear.php
@@ -55,7 +55,7 @@ class MonthYear extends \PHPFUI\Base
 		$array = \explode('/', \str_replace(['-',
 			'.',
 			'\\',
-			' ', ], '/', $value));
+			' ', ], '/', $value ?? ''));
 
 		if (3 == \count($array))
 			{
