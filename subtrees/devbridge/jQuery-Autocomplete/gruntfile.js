a62aa95c1d53bd67e7076b41f11896d126513136
Bruce Wells <brucekwells@gmail.com>
2025-11-17 11:35:07 -0500

    Revert "Merge commit '4e6f6a242df03ea8ac1acb8f86e00c6b9ffaf705' into master"
    This reverts commit 71464fb76ea38b577fd1edb05c7cd71d2269d107, reversing
    changes made to c32a8a565b342b96edeff40ca38e8dd4c3082c46.

--- subtrees/devbridge/jQuery-Autocomplete/gruntfile.js
+++ subtrees/devbridge/jQuery-Autocomplete/gruntfile.js
@@ -1,54 +1,58 @@
-module.exports = function (grunt) {
-    var pkg = grunt.file.readJSON("package.json");
+module.exports = function(grunt) {
 
-    var banner =
-        [
-            "/**",
-            "*  Ajax Autocomplete for jQuery, version " + pkg.version,
-            "*  (c) 2025 Tomas Kirda",
-            "*",
-            "*  Ajax Autocomplete for jQuery is freely distributable under the terms of an MIT-style license.",
-            "*  For details, see the web site: https://github.com/devbridge/jQuery-Autocomplete",
-            "*/",
-        ].join("\n") + "\n";
+  var pkg = grunt.file.readJSON('package.json');
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: pkg,
-        uglify: {
-            options: { banner: banner },
-            build: { src: "src/jquery.autocomplete.js", dest: "dist/jquery.autocomplete.min.js" },
-        },
-    });
+  var banner = [
+      '/**',
+      '*  Ajax Autocomplete for jQuery, version ' + pkg.version, 
+      '*  (c) 2017 Tomas Kirda',
+      '*',
+      '*  Ajax Autocomplete for jQuery is freely distributable under the terms of an MIT-style license.',
+      '*  For details, see the web site: https://github.com/devbridge/jQuery-Autocomplete',
+      '*/'].join('\n') + '\n';
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks("grunt-contrib-uglify");
+  // Project configuration.
+  grunt.initConfig({
+    pkg: pkg,
+    uglify: {
+      options: {
+        banner: banner
+      },
+      build: {
+        src: 'src/jquery.autocomplete.js',
+        dest: 'dist/jquery.autocomplete.min.js'
+      }
+    }
+  });
 
-    // Default task(s).
-    grunt.registerTask("default", ["uglify"]);
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.task.registerTask("build", "Create release", function () {
-        var version = pkg.version,
-            src = grunt.file.read("src/jquery.autocomplete.js").replace("%version%", version),
-            filePath = "dist/jquery.autocomplete.js";
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
 
-        // Update not minimized release version:
-        console.log("Updating: " + filePath);
-        grunt.file.write(filePath, src);
+  grunt.task.registerTask('build', 'Create release', function() {
+    var version = pkg.version,
+        src = grunt.file.read('src/jquery.autocomplete.js').replace('%version%', version),
+        filePath = 'dist/jquery.autocomplete.js';
 
-        // Minify latest version:
-        grunt.task.run("uglify");
+    // Update not minimized release version:
+    console.log('Updating: ' + filePath);
+    grunt.file.write(filePath, src);
 
-        // Update plugin version:
-        filePath = "devbridge-autocomplete.jquery.json";
-        src = grunt.file.readJSON(filePath);
+    // Minify latest version:
+    grunt.task.run('uglify');
 
-        if (src.version !== version) {
-            src.version = version;
-            console.log("Updating: " + filePath);
-            grunt.file.write(filePath, JSON.stringify(src, null, 4));
-        } else {
-            console.log("No updates for: " + filePath);
-        }
-    });
+    // Update plugin version:
+    filePath = 'devbridge-autocomplete.jquery.json';
+    src = grunt.file.readJSON(filePath);
+
+    if (src.version !== version){
+      src.version = version;
+      console.log('Updating: ' + filePath);
+      grunt.file.write(filePath, JSON.stringify(src, null, 4));
+    } else {
+      console.log('No updates for: ' + filePath);
+    }
+  });
 };
